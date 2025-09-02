20df9062070d66295c50bca61e97e6463248a90d
AICO24 <168385515+AICO24@users.noreply.github.com>
2025-09-02 15:41:41 +0800

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<!-- sidebar.php -->
+<aside class="sidebar">
+    <h2><i class="fa-solid fa-landmark"></i> JAIL MANAGEMENT</h2>
+    <ul>
+        <li><a href="dashboard.php"><i class="fa-solid fa-chart-line"></i><span>Dashboard</span></a></li>
+        <li><a href="booking.php" style="text-decoration:none;color:inherit;"><i class="fa-solid fa-book"></i> Booking</a></li>
+        <li><i class="fa-solid fa-user-shield"></i> Inmate Management</li>
+        <li><i class="fa-solid fa-clock"></i> Sentence Tracking</li>
+        <li><i class="fa-solid fa-door-closed"></i> Cell Management</li>
+        <li><i class="fa-solid fa-users"></i> Visitor Management</li>
+        <li><i class="fa-solid fa-calendar-days"></i> Event Management</li>
+        <li><i class="fa-solid fa-user-tie"></i> Staff Management</li>
+        <li><i class="fa-solid fa-chart-pie"></i> Analytics</li>
+        <li><i class="fa-solid fa-right-from-bracket"></i> <a href="logout.php" style="text-decoration:none;color:inherit;">Logout</a></li>
+    </ul>
+</aside>
